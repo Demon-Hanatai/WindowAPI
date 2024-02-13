e093db9c84ebd4187c97be8e545392a25c2bb69b
DEMON <Demonfood520@gmail.com>
2024-02-13 07:45:39 -0500

    Testing

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -1,14 +1,12 @@
-﻿
-using static WindowAPI.Winbase.h.Functions;
-using static WindowAPI.winuser.h.Functions;
-using static WindowAPI.wingdi.Functions;
-using System;
+﻿using WindowAPI.windef.Structures;
 using WindowAPI.winuser.h.Structures;
+using static WindowAPI.wingdi.Functions;
+using static WindowAPI.winuser.h.Functions;
 
 class Program
 {
-    
-    static void Main()
+
+    static unsafe void Main()
     {
         IntPtr d = CreateWindowExW(
             0, // Extended window style
@@ -24,28 +22,25 @@ class Program
             IntPtr.Zero, // Instance handle
             IntPtr.Zero); // Additional application data
 
-         // SW_SHOWNORMAL = 1, show the window
+        // SW_SHOWNORMAL = 1, show the window
         uint WS_VISIBLE = 0x10000000;
         uint WS_TABSTOP = 0x00010000;
         uint WS_CHILD = 0x40000000;
-        nint m = CreateWindowExW(0, "Button", "OK", WS_VISIBLE|WS_TABSTOP|WS_CHILD, 20, 20, 40, 50, d, 0, 0, 0);
+        nint m = CreateWindowExW(0, "Button", "OK", WS_VISIBLE | WS_TABSTOP | WS_CHILD, 20, 20, 40, 50, d, 0, 0, 0);
         int blueColor = 0xFF0000; // Blue color in RGB
         IntPtr hBrush = CreateSolidBrush(blueColor);
         ShowWindow(d, 1);
         ShowWindow(m, 1);
-        if (IntPtr.Size == 8)
-        {
-            // 64-bit application
-            SetClassLongPtrA(d, -10, hBrush.ToInt32());
-        }
-        else
-        {
-            // 32-bit application
-            SetClassLongPtrA(d, -10, hBrush.ToInt32());
-        }
 
-       
-        while (GetMessageA(out MSG de, d, 0, 0)&& GetMessageA(out MSG e, m, 0, 0))
+
+        SetClassLongPtrA(d, -10, hBrush.ToInt32());
+        var mee = new RECT();
+        InvalidateRect(d, *(RECT*)&mee, true);
+        UpdateWindow(d);
+        UpdateColors(d);
+
+
+        while (GetMessageA(out MSG de, d, 0, 0))
         {
 
         }
